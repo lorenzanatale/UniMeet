ee50d49156d6656469f267e2c4f7a1d4e429a89a
cirodanzilli <cirodanzilli@localhost>
2024-12-27 13:29:51 +0100

    Update database.

--- UniMeet/UniMeet.sql
+++ UniMeet/UniMeet.sql
@@ -9,7 +9,7 @@ cognome VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 passwordHash VARCHAR(255) NOT NULL,
 domandaSicurezza VARCHAR(200) NOT NULL,
-risposta VARCHAR(30) NOT NULL
+risposta VARCHAR(200) NOT NULL
 );
 CREATE TABLE  professore(
 codice VARCHAR(30) PRIMARY KEY NOT NULL,
@@ -19,12 +19,12 @@ ufficio VARCHAR(30) NOT NULL,
 email VARCHAR(30) NOT NULL,
 passwordHash VARCHAR(255) NOT NULL,
 domandaSicurezza VARCHAR(200) NOT NULL,
-risposta VARCHAR(30) NOT NULL
+risposta VARCHAR(200) NOT NULL
 );
 CREATE TABLE insegnamento(
     codice INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(30) NOT NULL,
-    codiceProfessore VARCHAR(30) NOT NULL,
+    codiceProfessore VARCHAR(50) NOT NULL,
     FOREIGN KEY(codiceProfessore) REFERENCES professore(codice) 
 );
 
@@ -33,7 +33,7 @@ CREATE TABLE ricevimento(
     giorno VARCHAR(30) NOT NULL,
     ora VARCHAR(30) NOT NULL,
     note VARCHAR(200) NOT NULL,
-    codiceProfessore VARCHAR(30) NOT NULL,
+    codiceProfessore VARCHAR(50) NOT NULL,
     FOREIGN KEY(codiceProfessore) REFERENCES professore(codice)
 );
 
@@ -42,8 +42,8 @@ CREATE TABLE prenotazioneRicevimento(
     giorno VARCHAR(30) NOT NULL,
     ora VARCHAR(30) NOT NULL,
     note VARCHAR(200) NOT NULL,
-    codiceProfessore VARCHAR(30) NOT NULL,
-    matricolaStudente VARCHAR(30) NOT NULL,
+    codiceProfessore VARCHAR(50) NOT NULL,
+    matricolaStudente VARCHAR(50) NOT NULL,
     FOREIGN KEY (codiceProfessore) REFERENCES professore(codice),
     FOREIGN KEY (matricolaStudente) REFERENCES studente(matricola)
 );
